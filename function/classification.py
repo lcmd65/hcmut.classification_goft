a36763f3a7aaa8264e8b395f82a613442d032043
DatLeChonMinh <dat.lemindast@gmail.com>
2023-03-13 02:21:47 +0700

    change

--- function/classification.py
+++ function/classification.py
@@ -1,1 +1,8 @@
 import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
